35a727242140e57ed7117fdb28b1e3f4ba567cd1
Brad-Duderstadt <brad.duderstadt@coxautoinc.com>
2021-01-23 10:00:34 -0600

    Finished up initial version of node / express server by adding listener

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,7 @@ const PORT = 3000;
 app.get("/", (req, res) => {
   res.send(`Our Soccer application is running on port: ${PORT}`);
 });
+
+app.listen(PORT, () => {
+  console.log(`Your soccer server is running on port: ${PORT}`);
+})
